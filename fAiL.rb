483d9c7d05dbfa5ea18b86ffcb8cc88d36925f49
Andrew Orr <andrew@andreworr.ca>
2013-02-06 17:04:40 -0600

    Update fAiL.rb

--- fAiL.rb
+++ fAiL.rb
@@ -1,4 +1,3 @@
-puts 'djsbx is a noob'
 5000000.times { |pos|
   file = (0..5).map{ ([*('a'..'z'),*('A'..'Z'),*('0'..'9')])[rand(62)] }.join
   puts "hit: #{file} pos: #{pos}" if file.downcase.include? 'fail'
