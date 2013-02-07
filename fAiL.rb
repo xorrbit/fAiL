f07ebbcd47d82ff04e8eddf3593fbec5658ad0f4
Andrew Orr <aorr@iders.ca>
2013-02-06 19:02:37 -0600

    Moved alphanum array generation out of the loop as per YoMotha's suggestion, results in massive speedup

--- fAiL.rb
+++ fAiL.rb
@@ -1,5 +1,7 @@
+alphanum = ([*('a'..'z'),*('A'..'Z'),*('0'..'9')])
+
 5000000.times { |pos|
-  file = (0..5).map{ ([*('a'..'z'),*('A'..'Z'),*('0'..'9')])[rand(62)] }.join
+  file = (0..5).map{ alphanum[rand(62)] }.join
   puts "hit: #{file} pos: #{pos}" if file.downcase.include? 'fail'
 }
 
