64ebe48b4a2f12168fbf75993d9838591b24e6eb
Fernando Chorney <fernando.chorney@telenium.ca>
2013-02-07 10:07:42 -0600

    Switched the large range to an xrange to speed it up

--- fAiL.py
+++ fAiL.py
@@ -1,11 +1,11 @@
 import random
 from string import ascii_letters, digits
 
-RANGE_MAX = 5000000
-CHARS_MAX = 6
+RANGE_MAX = xrange(0, 5000000)
+CHARS_MAX = range(6)
 ALPHANUM = ascii_letters + digits
 
-for pos in range(0,RANGE_MAX):
-    val = ''.join(random.choice(ALPHANUM) for x in range(CHARS_MAX))
+for pos in RANGE_MAX:
+    val = ''.join(random.choice(ALPHANUM) for x in CHARS_MAX)
     if 'fail' in val.lower():
         print "hit: %s pos: %s" % (val, pos)
